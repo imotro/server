736d16c5c87c7fe4aab9c79c0f425bf6deddb36a
imotro <139806892+imotro@users.noreply.github.com>
2024-03-30 10:32:34 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# Define commands
 command1="cd bungee && sudo java -jar bungee.jar"
 command2="cd server && sudo java -jar server.jar"
 
 # Run commands concurrently
-eval "$command1" &
-eval "$command2" &
+bash -c "$command1" &
+bash -c "$command2" &
 
+# Wait for both commands to finish
 wait
 
 echo "Both tasks have finished."
